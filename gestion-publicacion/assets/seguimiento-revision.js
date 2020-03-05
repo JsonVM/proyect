cbcbb7bf54f7abb3cb9d7dc91c11d2367e447793
fer9972 <50627246+fer9972@users.noreply.github.com>
2020-03-05 06:45:33 -0500

    Documentacion del codigo

--- gestion-publicacion/assets/seguimiento-revision.js
+++ gestion-publicacion/assets/seguimiento-revision.js
@@ -6,7 +6,7 @@ export default {
     data() {
         return {
             enEdicion: false,
-
+            //se guardan todos los seguimientos nuevos que se ingresan 
             seg: {
                 tarea: "",
                 fecha: "",
@@ -14,7 +14,7 @@ export default {
                 estado: "",
                 acciones: true
             },
-
+            //se inicializa el array seguimientos para luego guardar todos los seguimientos ahi
             lista_seguimientos: [
                 {
                     tarea: "001",
@@ -36,10 +36,12 @@ export default {
         }
 
     },
+    //aca apenas se carga la pagina se llama el metodo para guardar en el local storage
     mounted() {
         this.local()
     },
     methods: {
+      //creamos los seguimientos y los añadimos al array
         crearSeguimiento() {
             this.lista_seguimientos.push(this.seg);
             this.seg = {
@@ -51,6 +53,7 @@ export default {
             };
             localStorage.setItem('seguimientos', JSON.stringify(this.lista_seguimientos));
         },
+        //eliminanos un seguimiento segun el item que se le pase por parametro
         eliminarSeguimiento({ item }) {
             let posicion = this.lista_seguimientos.findIndex(
                 seg => seg.tarea == item.tarea
@@ -58,6 +61,7 @@ export default {
             this.lista_seguimientos.splice(posicion, 1);
             localStorage.setItem('seguimientos', JSON.stringify(this.lista_seguimientos));
         },
+        //este metodo nos pone en el formulario todos los datos del seguimiento que quieren editar
         cargarSeguimiento({ item }) {
             let se = this.lista_seguimientos.find(
                 seg => seg.tarea == item.tarea
@@ -66,6 +70,8 @@ export default {
             this.seg = Object.assign({}, se);
             localStorage.setItem('seguimientos', JSON.stringify(this.lista_seguimientos));
         },
+        //estes metodo se llama luego de que se hace un cambio en un seguimiento ya existente,
+        //para que quede guardado con sus nuevos valores
         actualizarSeguimiento() {
             let seg1 = this.lista_seguimientos.findIndex(
                 seg => seg.tarea == this.seg.tarea
@@ -81,8 +87,8 @@ export default {
             };
             localStorage.setItem('seguimientos', JSON.stringify(this.lista_seguimientos));
         },
+        //metodo para guardar la lista de seguimientos en el local storage
         local() {
-
             var datosLocal = JSON.parse(localStorage.getItem('seguimientos'));
             if (datosLocal === null) {
                 this.lista_seguimientos = [];
