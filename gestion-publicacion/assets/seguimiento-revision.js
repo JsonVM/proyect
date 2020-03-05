0df70ca7a1f3fb3758b22ab085db5e13d854066a
JsonVM <48698637+JsonVM@users.noreply.github.com>
2020-03-04 22:58:40 -0500

    cambio javascript de seguimiento

--- gestion-publicacion/assets/seguimiento-revision.js
+++ gestion-publicacion/assets/seguimiento-revision.js
@@ -1,73 +1,87 @@
 export default {
     data() {
         return {
-        enEdicion: false,
+            enEdicion: false,
 
-          seg: {
-            tarea: "",
-            fecha: "",
-            comentario: "",
-            estado: "",
-            acciones: true
-          },
-          
-        lista_seguimientos: [
-          {
-            tarea: "001",
-            fecha: "04/03/2020",
-            comentario: "Buen trabajo en general",
-            estado: "Pendiente",
-            acciones: true
-          },
-          {
-            tarea: "002",
-            fecha: "04/03/2020",
-            comentario: "Hay que realizar correcciones ortográficas",
-            estado: "No aprobado",
-            acciones: true
-          }
-        ]
+            seg: {
+                tarea: "",
+                fecha: "",
+                comentario: "",
+                estado: "",
+                acciones: true
+            },
 
-        ,show:true
-      };
+            lista_seguimientos: [
+                {
+                    tarea: "001",
+                    fecha: "04/03/2020",
+                    comentario: "Buen trabajo en general",
+                    estado: "Pendiente",
+                    acciones: true
+                },
+                {
+                    tarea: "002",
+                    fecha: "04/03/2020",
+                    comentario: "Hay que realizar correcciones ortográficas",
+                    estado: "No aprobado",
+                    acciones: true
+                }
+            ]
+
+            , show: true
+        };
     },
     methods: {
         crearSeguimiento() {
-          this.lista_seguimientos.push(this.seg);
-          this.Seg = {
-            tarea: "",
-            fecha: "",
-            comentario: "",
-            estado: "",
-            acciones: true
-          };
+            this.lista_seguimientos.push(this.seg);
+            this.seg = {
+                tarea: "",
+                fecha: "",
+                comentario: "",
+                estado: "",
+                acciones: true
+            };
         },
         eliminarSeguimiento({ item }) {
             let posicion = this.lista_seguimientos.findIndex(
-              seg => seg.titulo == item.tarea
+                seg => seg.tarea == item.tarea
             );
             this.lista_seguimientos.splice(posicion, 1);
         },
         cargarSeguimiento({ item }) {
-          let se = this.lista_seguimientos.find(
-            seg => seg.tarea == item.tarea
-          );
-          this.enEdicion = true;
-          this.seg = Object.assign({}, se);
+            let se = this.lista_seguimientos.find(
+                seg => seg.tarea == item.tarea
+            );
+            this.enEdicion = true;
+            this.seg = Object.assign({}, se);
         },
         actualizarSeguimiento() {
-          let seg1 = this.lista_seguimientos.findIndex(
-            seg => seg.tarea == this.seg.tarea
-          );
-          this.lista_seguimientos.splice(seg1, 1, this.seg);
-          this.seg = {
-            tarea: "",
-            fecha: "",
-            comentario: "",
-            estado: "",
-            acciones: true
-          };
-    
+            let seg1 = this.lista_seguimientos.findIndex(
+                seg => seg.tarea == this.seg.tarea
+            );
+            this.enEdicion = false;
+            this.lista_seguimientos.splice(seg1, 1, this.seg);
+            this.seg = {
+                tarea: "",
+                fecha: "",
+                comentario: "",
+                estado: "",
+                acciones: true
+            };
+
         }
-      }
-    };
+    }/**,
+    created: function(){
+        if (typeof window !== 'undefined') {
+            var datosLocal = JSON.parse(localStorage.getItem('seguimiento'));
+        }
+
+        if (typeof window !== 'undefined') {
+            if (datosLocal === null) {
+                this.lista_seguimientos = [];
+            } else {
+                this.lista_seguimientos = datosLocal;
+            }
+        }
+    }*/
+};
