7e849b2525228c777966d052158bb76ee0fc5e62
JsonVM <48698637+JsonVM@users.noreply.github.com>
2020-05-02 15:00:24 -0500

    se añade el servicio de postgress

---
+++ backend/services/postgres.js
@@ -0,0 +1,21 @@
+const { Pool } = require("pg");
+
+class ServicioPG {
+  constructor() {
+    this.pool = new Pool({
+      user: "postgres",
+      host: "localhost",
+      //AÑADIR!!!
+      database: "",
+      password: "",
+      port: 5432
+    });
+  }
+
+  async ejecutarSql(sql) {
+    let respuesta = await this.pool.query(sql);
+    return respuesta;
+  }
+}
+
+module.exports = ServicioPG;
