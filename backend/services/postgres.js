ceb4580481390f0c7c5342762d02e86dfc2206d2
Kejuu <kevin11062011@gmail.com>
2020-05-09 19:43:17 -0500

    Generación de pdf prueba

--- backend/services/postgres.js
+++ backend/services/postgres.js
@@ -3,12 +3,11 @@ const { Pool } = require("pg");
 class ServicioPG {
   constructor() {
     this.pool = new Pool({
-      user: "postgres",
-      host: "localhost",
-      //AÑADIR!!!
-      database: "",
-      password: "",
-      port: 5432
+      user: "dllo_web_udem",
+      host: "saurmo.com",
+      database: "udem",
+      password: "bf5e722cc518fce3c4a57fdb1b6647b0434138370eb1c30f9293ec8e03062b78",
+      port: 5432,
     });
   }
 
